dbb95be9974b1927e61af14d13de5072057ed0d1
Nick Gerrard <nick.gerrard16@gmail.com>
2026-05-18 10:49:04 -0400

    add border to recipe card

--- frontend/src/lib/components/RecipeCard.svelte
+++ frontend/src/lib/components/RecipeCard.svelte
@@ -15,8 +15,9 @@
 
 <button
     onclick={onclick}
-    class="w-full text-left border border-white/30 bg-white/10 backdrop-blur-md rounded-lg shadow-lg flex flex-col gap-4 p-8 hover:scale-105 transition-all duration-200"
+    class="overflow-hidden w-full text-left border border-white/30 bg-white/10 backdrop-blur-md rounded-lg shadow-lg flex flex-col gap-4 p-8 hover:scale-105 transition-all duration-200"
 >
+  <div class="h-2 -mx-8 -mt-8 bg-gradient-to-r from-fuchsia-400 via-green-400 to-violet-400"></div>
     <h1 class="text-white text-3xl font-bold">{recipe.name}</h1>
     <p class="text-sm text-white">{recipe.description}</p>
     <div class="mt-auto flex justify-center gap-4">
